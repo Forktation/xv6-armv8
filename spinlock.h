7577cc938f2618586b0745eff576d5f8488038c4
Jasen Qin <capitalswine@gmail.com>
2023-09-01 19:10:13 +1000

    ADDADD

--- spinlock.h
+++ spinlock.h
@@ -1,12 +1,12 @@
 // Mutual exclusion lock.
 struct spinlock {
-    uint locked;
+  uint locked;
 
-    // For debugging:
-    char* name;
-     // The cpu holding the lock.
-    struct cpu* cpu;
-    // The call stack (an array of program counters) that locked the lock.
-    // Why is it called a call stack, like a pointer or something to the core's actual stack?
-    uint pcs[10];
+  // For debugging:
+  char* name;
+  // The cpu holding the lock.
+  struct cpu* cpu;
+  // The call stack (an array of program counters) that locked the lock.
+  // Why is it called a call stack, like a pointer or something to the core's actual stack?
+  uint pcs[10];
 };
